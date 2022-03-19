48bdd53539c767d1ed0a2d1fd132166dbf224d97
Боил Василев <boil4o@abv.bg>
2022-03-19 18:57:23 +0200

    #28ywwah - fix console namespace

--- NatureSim.Console/Program.cs
+++ NatureSim.Console/Program.cs
@@ -1,12 +1,11 @@
-﻿using System;
-
+﻿
 namespace NatureSim.Console
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Nature Simulator!");
+            System.Console.WriteLine("Hello Nature Simulator!");
         }
     }
 }
