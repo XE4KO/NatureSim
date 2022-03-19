ab7f6542ef1b761b41eec22191ff3e4980e5c3f8
Boil4 <Boil4@DESKTOP-5HM98JU>
2022-03-19 18:40:06 +0200

    Add project files.

---
+++ NatureSim.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NatureSim.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Nature Simulator!");
+        }
+    }
+}
