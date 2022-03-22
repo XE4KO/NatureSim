cc8971da4f89ed304a19108c37f79b8c36cd063e
Боил Василев <boil4o@abv.bg>
2022-03-22 16:54:33 +0200

    Initialized animals and food

--- NatureSim.Console/Program.cs
+++ NatureSim.Console/Program.cs
@@ -1,11 +1,39 @@
 ﻿
 namespace NatureSim.Console
 {
+    class Wolf
+    {
+        public Wolf()
+        {
+            int wolfHealth = 10;
+            string[] wolfDiet = {"meat", "fish"};
+        }
+    }
+    class Bear
+    {
+        public Bear()
+        {
+            int bearHealth = 15;
+            string[] bearDiet = {"meat", "fish", "berry"};
+        }
+    }
+    class Bunny
+    {
+        public Bunny()
+        {
+            int bunnyHealth = 5;
+            string[] bunnyDiet = {"berry", "carrot"};
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello Nature Simulator!");
+            Bunny bunny = new Bunny();
+            Wolf wolf = new Wolf();
+            Bear bear = new Bear();
+            string[] food = {"berry", "carrot", "fish", "meat", "nothing"};
+            System.Console.WriteLine("Hello to Nature Simulator!");
         }
     }
 }
