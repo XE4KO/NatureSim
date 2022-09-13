dc12e8a031fe82b5fdb33a4f4db41d8d6a90ba10
Боил Василев <boil4o@abv.bg>
2022-09-13 18:18:52 +0300

    git commit -m "CU-28ywyyj - Animals find food depedning of the tile"

--- NatureSim.Console/Animal.cs
+++ NatureSim.Console/Animal.cs
@@ -8,21 +8,20 @@ namespace NatureSim.Console
     {
         private readonly string animalType;
         private int health;
-        //private readonly string[] edibleFood;
         private readonly HashSet<Foods> diet;
         private int starve;
-        private int coordsX;
-        private int coordsY;
+        public int coordsX;
+        public int coordsY;
         private Random random = new Random();
         Map map = new Map();
-        public Animal(string animalType, int health, int starve, IEnumerable<Foods> diet, Biome[,] mapBiomes)
+        public Animal(string animalType, int health, int starve, IEnumerable<Foods> diet)
         {
             this.animalType = animalType;
             this.health = health;
             this.starve = starve;
             this.diet = new HashSet<Foods>(diet);
-            this.coordsX = random.Next(map.width);
-            this.coordsY = random.Next(map.height);
+            this.coordsX = random.Next(map.Width);
+            this.coordsY = random.Next(map.Height);
         }
 
         public bool IsAlive => health > 0;
@@ -45,6 +44,7 @@ namespace NatureSim.Console
                         System.Console.Write($"{animalType} does not eat {food.foodName} and died");
                         System.Console.BackgroundColor = System.ConsoleColor.Black;
                         System.Console.WriteLine(" ");
+                        System.Console.ReadKey();
                     }
                     else
                         System.Console.WriteLine($"{animalType} does not eat {food.foodName} and is left with {health} health.");
@@ -53,25 +53,12 @@ namespace NatureSim.Console
         }
         public void Move()
 		{
-            coordsX += random.Next(3) - 1;
-            coordsY += random.Next(3) - 1;
-			if (coordsX > map.width)
-			{
-                coordsX = 0;
-			}
-            if (coordsX < 0)
-            {
-                coordsX = map.width;
-            }
-            if (coordsY > map.height)
-            {
-                coordsY = 0;
-            }
-            if (coordsY < 0)
-            {
-                coordsY = map.height;
-            }
-            //System.Console.WriteLine($"{animalType} moved to [{coordsX}:{coordsY}].");
-        }
-    }
+			coordsX += random.Next(3) - 1;
+			coordsY += random.Next(3) - 1;
+            coordsX = map.LimitX(coordsX);
+            coordsY = map.LimitY(coordsY);
+			//System.Console.WriteLine($"{animalType} moved to [{coordsX}:{coordsY}].");
+		}
+
+	}
 }
--- NatureSim.Console/Biome.cs
+++ NatureSim.Console/Biome.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace NatureSim.Console
 {
-    record Biome(Food[] availableFoods);
-    record Field() : Biome(new Food[] {     new Grass(),    new Meat(),     new Carrot() });
+	record Biome(Food[] availableFoods)
+	{
+		static readonly Random random = new();
+
+		internal Food FindFood()
+			=> availableFoods[random.Next(availableFoods.Length)];
+	}
+
+	record Field() : Biome(new Food[] {     new Grass(),    new Meat(),     new Carrot() });
     record Forest() : Biome(new Food[] {    new Berry(),    new Meat(),     new Grass(),    new Carrot(), new Acorn(), new Mouse() });
     record Mountain() : Biome(new Food[] {  new Nothing(),  new Nothing(),  new Grass(),    new Berry() });
     record River() : Biome(new Food[] {     new Fish(),     new Fish(),     new Nothing(),  new Nothing() });
